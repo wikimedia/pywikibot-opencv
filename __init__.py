154d9f1f254d12cddce1ee21fa0c581b57f2a99b
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2013-06-01 15:51:27 +0000

    new feature; python wrapper to opencv POSIT alogrithm
    git-svn-id: https://svn.toolserver.org/svnroot/drtrigon/externals/opencv@564 f7c66474-1b4d-40db-9de1-ad65934439f2

--- __init__.py
+++ __init__.py
@@ -3,6 +3,15 @@ import sys, os
 scriptdir = os.path.dirname(sys.argv[0])
 if not os.path.isabs(scriptdir):
     scriptdir = os.path.abspath(os.path.join(os.curdir, scriptdir))
+#sys.path.append("/home/ursin/data/toolserver/pywikipedia/dtbext/opencv")
+    
+libdir = os.path.join(scriptdir, 'dtbext/opencv')
+if not os.path.exists(libdir):
+    os.makedirs(libdir)
+# path has to exist BEFORE appending, otherwise the re-import fails
+sys.path.append(libdir)
+
+import numpy
 
 try:
     # try to import
@@ -14,8 +23,8 @@ except ImportError, e:
     os.chdir( os.path.join(scriptdir, 'dtbext/opencv') )
 
     # remove/reset if existing already
-    if os.path.exists('BoWclassify.so'):
-        os.remove('BoWclassify.so')
+    if os.path.exists(os.path.join(libdir, 'BoWclassify.so')):
+        os.remove( os.path.join(libdir, 'BoWclassify.so') )
 
     # compile python module (may be use 'distutil' instead of 'make' here)
     if os.system("make BoWclassify.so"):
@@ -25,3 +34,42 @@ except ImportError, e:
 
     # re-try to import
     import BoWclassify
+
+try:
+    # try to import
+    import posit as _posit
+except ImportError, e:
+    print "(re-)compilation triggered because of: '%s'" % e
+
+    cur = os.path.abspath(os.curdir)
+    os.chdir( os.path.join(scriptdir, 'dtbext/opencv') )
+
+    # remove/reset if existing already
+    if os.path.exists(os.path.join(libdir, 'posit.so')):
+        os.remove( os.path.join(libdir, 'posit.so') )
+
+    # compile python module (may be use 'distutil' instead of 'make' here)
+    if os.system("make posit.so"):
+        raise ImportError("'posit.so' could not be compiled!")
+
+    os.chdir( cur )
+
+    # re-try to import
+    import posit as _posit
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+def posit(model, image, crit):
+    _model = [ tuple(item) for item in model.astype(float) ]
+    _image = [ tuple(item) for item in image.astype(float) ]
+    #_crit  = tuple(crit.astype(float))
+    _crit  = (long(crit[0]), float(crit[1]))
+
+    try:
+        (_rmat, _tvec) = _posit.main(_model, _image, _crit)
+    except Exception, e:
+        raise e.__class__("Error in 'cvPOSIT' wrapper 'posit.so' (%s)" % e)
+
+    rmat = numpy.reshape(numpy.array(_rmat), (3,3))
+    tvec = numpy.array(_tvec)
+    return (rmat, tvec)
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 
 all: build test
 
-build: BoWclassify.so BoWclassify
+build: BoWclassify.so BoWclassify posit.so
 
 BoWclassify.so: bagofwords_classification_python.cpp
 	g++ bagofwords_classification_python.cpp `pkg-config --libs --cflags opencv python` -lboost_python -o BoWclassify.so -shared -fPIC
@@ -9,11 +9,15 @@ BoWclassify.so: bagofwords_classification_python.cpp
 BoWclassify: bagofwords_classification_python.cpp
 	g++ bagofwords_classification_python.cpp `pkg-config --libs --cflags opencv python` -lboost_python -o BoWclassify
 
+posit.so: posit_python.cpp
+	g++ posit_python.cpp `pkg-config --libs --cflags opencv python` -lboost_python -o posit.so -shared -fPIC
+
 test: build
 	-./BoWclassify
 	python -c "import BoWclassify; print BoWclassify.main(0, '', '', '', '', '', [])"
 	python -c "import BoWclassify; print BoWclassify.main(0, '', '', '', '', '', [str(u'äbc'.encode('latin-1'))])"
+	python -c "import posit; print posit.main([], [], ())"
 
 clean:
-	-rm BoWclassify.so BoWclassify
+	-rm BoWclassify.so BoWclassify posit.so
 
