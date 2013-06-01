d90ea9e11bfe3f1a984cb8f20aa9a845c378ccef
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2013-06-01 17:42:38 +0000

    minor changes and improvements
    git-svn-id: https://svn.toolserver.org/svnroot/drtrigon/externals/opencv@568 f7c66474-1b4d-40db-9de1-ad65934439f2

--- __init__.py
+++ __init__.py
@@ -59,8 +59,9 @@ except ImportError, e:
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
 def posit(model, image, crit):
-    _model = [ tuple(item) for item in model.astype(float) ]
-    _image = [ tuple(item) for item in image.astype(float) ]
+# TODO: '_model' translation to origin needed? since first point has to be (0, 0, 0) ... ?!?
+    _model = [ tuple(item) for item in numpy.array(model).astype(float) ]
+    _image = [ tuple(item) for item in numpy.array(image).astype(float) ]
     #_crit  = tuple(crit.astype(float))
     _crit  = (long(crit[0]), float(crit[1]))
 
@@ -72,3 +73,27 @@ def posit(model, image, crit):
     rmat = numpy.reshape(numpy.array(_rmat), (3,3))
     tvec = numpy.array(_tvec)
     return (rmat, tvec)
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+def unit_test():
+    print "TEST 'BoWclassify'"
+    pass
+
+    print "TEST 'posit'"
+    cubeSize = 10.0
+    model  = [(0.0, 0.0, 0.0),
+              (0.0, 0.0, cubeSize),
+              (cubeSize, 0.0, 0.0),
+              (0.0, cubeSize, 0.0),]
+    image  = [( -48, -224),
+              (-287, -174),
+              ( 132, -153),
+              ( -52,  149),]
+    crit  = (100, 1.0e-4)
+    #model = np.array(model)
+    #image = np.array(image)
+    #crit  = np.array(crit)
+    (rmat, tvec) = posit(model, image, crit)
+    print rmat
+    print tvec
